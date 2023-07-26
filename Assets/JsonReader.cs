5732e743e0cb72a12b1eb3b433a2c44d889f26da
bill <truongbill003@gmail.com>
2023-07-26 20:46:36 +0700

    bill update

--- Assets/JsonReader.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-public class JsonReader : MonoBehaviour
-{
-    [SerializeField] private QuestionConfig config;
-
-
-}
--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -24,9 +24,6 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Room camera
-        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
-
         //Follow player
         transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
         lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
