5ca80108a6f41407cf4b51dd503132206c1ce37c
Hoàng Ngọc Dung <dunghoang0401237@gmail.com>
2023-08-02 16:41:32 +0700

    hihi fix bug

--- Assets/section/gameManager.cs
+++ Assets/section/gameManager.cs
@@ -1,3 +1,206 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using TMPro;
+// using System;
+// using UnityEngine.UI;
+
+// namespace section
+// {
+//     [Serializable]
+
+//     public class QuestionData
+//     {
+//         public string question;
+//         public string ansA;
+//         public string ansB;
+//         public string ansC;
+//         public string ansD;
+//         public string correctAns;
+//     }
+//     public enum GameState
+//     {
+//         Home,
+//         Gameplay,
+//         Credit,
+//         Learn,
+//         GameWin,
+//         Gameover
+//     }
+
+//     public class gameManager: MonoBehaviour
+//     {
+//     // Start is called before the
+//         [SerializeField] private TextMeshProUGUI txtQuestion;
+//         [SerializeField] private TextMeshProUGUI txtAnswerA;
+//         [SerializeField] private TextMeshProUGUI txtAnswerB;
+//         [SerializeField] private TextMeshProUGUI txtAnswerC;
+//         [SerializeField] private TextMeshProUGUI txtAnswerD;
+//         [SerializeField] private Image  ImgAnsA;
+//         [SerializeField] private Image  ImgAnsB;
+//         [SerializeField] private Image  ImgAnsC;
+//         [SerializeField] private Image  ImgAnsD;
+//         [SerializeField] private AudioSource Audio;
+//         [SerializeField] private AudioClip CorectAns;
+//         [SerializeField] private AudioClip WrongAns;
+
+//         [SerializeField] private GameObject vt_HomePanel, vt_GamePanel, vt_GamoverPanel, vt_CreditPanel,vt_Learn,vt_Win;
+
+
+//         //[SerializeField] private QuestionData[] questionData;
+//         [SerializeField] private QuestionScriptableData[] questionData;
+        
+
+
+//         private int QuestionIndex;
+//         private GameState vt_GameState;
+//         public int vt_Live = 2;
+        
+//         void Start()
+//         {
+//             SetGameState(GameState.Home);
+            
+//             //SetGameState(GameState.Credit);
+//             QuestionIndex = 0;
+//             InitQuestion(0);    
+
+//         }
+
+        
+
+//     // Update is called once per frame
+//         void Update()
+//         {
+            
+//         }
+//         IEnumerator MyCoroutine()
+//         {
+//             yield return new WaitForSeconds(10f);
+//         }
+
+
+//         public void Ans_pressed(string SelectAns)
+//         {
+//             //StartCoroutine(MyCoroutine());
+//             bool flag = false;
+
+//             if (questionData[QuestionIndex].correctAns == SelectAns)
+//             {
+//                 flag = true;
+//                 Audio.PlayOneShot(CorectAns);
+//                 Debug.Log("10d gioi gioi");
+//                 SetGameState(GameState.GameWin);
+//             }
+//             else
+//             {
+//                 vt_Live--;
+//                 if (vt_Live == 0) 
+//                 {
+                    
+//                     SetGameState(GameState.Gameover);
+//                 }
+//                 flag = false;
+//                 Debug.Log("Ngouuuu");
+//             }
+//             Audio.PlayOneShot(WrongAns);
+//             switch(SelectAns)
+//             {
+//                 case "a":
+//                     ImgAnsA.color = flag ? Color.green : Color.red;
+//                     break;
+//                 case "b":
+//                     ImgAnsB.color = flag ? Color.green : Color.red;
+//                     break;
+//                 case "c":
+//                     ImgAnsC.color = flag ? Color.green : Color.red;
+//                     break;
+//                 case "d":
+//                     ImgAnsD.color = flag ? Color.green : Color.red;
+//                     break;
+//             }
+
+//             if (flag)
+//             {
+//                 if (QuestionIndex >= questionData.Length)
+//                 {
+//                     Debug.Log("Xin chuc mung! Ban da chien thang");
+//                     return;
+                    
+//                 }
+//                 QuestionIndex++;
+//                 InitQuestion(QuestionIndex);
+//             }
+//         }
+
+//         private void InitQuestion(int vt)
+//         {
+//             if (vt < 0 || vt >= questionData.Length) 
+//                 return;
+
+//             ImgAnsA.color = Color.white;
+//             ImgAnsB.color = Color.white;
+//             ImgAnsC.color = Color.white;
+//             ImgAnsD.color = Color.white;
+//             txtQuestion.text = questionData[vt].question;
+//             txtAnswerA.text ="A: " + questionData[vt].ansA;
+//             txtAnswerB.text ="B: " + questionData[vt].ansB;
+//             txtAnswerC.text ="C: " + questionData[vt].ansC;
+//             txtAnswerD.text ="D: " + questionData[vt].ansD;
+//         }
+
+//         public void SetGameState(GameState state)
+//         {
+//             vt_GameState = state;
+//             vt_Live = 2;
+//             QuestionIndex = 0;
+//             vt_HomePanel.SetActive(vt_GameState == GameState.Home);
+//             vt_GamePanel.SetActive(vt_GameState == GameState.Gameplay);
+//             vt_GamoverPanel.SetActive(vt_GameState == GameState.Gameover);
+//             vt_CreditPanel.SetActive(vt_GameState == GameState.Credit);
+//             vt_Learn.SetActive(vt_GameState == GameState.Learn);
+//             vt_Win.SetActive(vt_GameState == GameState.GameWin);
+//             //vt_GameoverPanel.SetActive(vt_GameState == GameState.Gameover);
+//         }
+
+//         public void BTnPlay_Pressed()
+//         {
+//             vt_Live = 2;
+//             //QuestionIndex = 0;
+//             SetGameState(GameState.Gameplay);
+//             InitQuestion(0);
+            
+//         }
+
+//         public void BtnHome_Pressed()
+//         {
+//             vt_Live = 2;
+//             SetGameState(GameState.Home);
+//         }
+//         public void BtnCredit_pressed()
+//         {
+//             SetGameState(GameState.Credit);
+//         }
+
+//         public void BtnLearn_pressed()
+//         {
+//             SetGameState(GameState.Learn);
+//         }
+
+//         public void OpenNet1()
+//         {
+//             Application.OpenURL("https://miro.com/app/board/uXjVO2n0xkg=/");
+//         }
+//         public void OpenNet2()
+//         {
+//             Application.OpenURL("https://miro.com/app/board/uXjVO2QL4po=/");
+//         }
+
+
+
+//     }
+// }
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -24,6 +227,7 @@ namespace section
         Gameplay,
         Credit,
         Learn,
+        GameWin,
         Gameover
     }
 
@@ -43,7 +247,7 @@ namespace section
         [SerializeField] private AudioClip CorectAns;
         [SerializeField] private AudioClip WrongAns;
 
-        [SerializeField] private GameObject vt_HomePanel, vt_GamePanel, vt_GamoverPanel, vt_CreditPanel,vt_Learn;
+        [SerializeField] private GameObject vt_HomePanel, vt_GamePanel, vt_GamoverPanel, vt_CreditPanel, vt_Learn, vt_Win;
 
 
         //[SerializeField] private QuestionData[] questionData;
@@ -69,10 +273,11 @@ namespace section
         {
             
         }
+        bool flag = false;
+
         public void Ans_pressed(string SelectAns)
         {
-            bool flag = false;
-
+            
             if (questionData[QuestionIndex].correctAns == SelectAns)
             {
                 flag = true;
@@ -105,17 +310,28 @@ namespace section
                     ImgAnsD.color = flag ? Color.green : Color.red;
                     break;
             }
+            Debug.Log(flag);
+            
             if (flag)
-            {
-                if (QuestionIndex >= questionData.Length)
+            {   
+                Debug.Log(flag);
+            
+                if (QuestionIndex == questionData.Length - 1)
                 {
                     Debug.Log("Xin chuc mung! Ban da chien thang");
-                    return;
+                    SetGameState(GameState.GameWin);
+                    //return;
                     
                 }
-                QuestionIndex++;
-                InitQuestion(QuestionIndex);
+                else Invoke("ChangeQuiz", 5);
             }
+            
+        }
+        private void ChangeQuiz()
+        {
+            QuestionIndex++;
+            InitQuestion(QuestionIndex);
+            flag = false;
         }
 
         private void InitQuestion(int vt)
@@ -143,6 +359,7 @@ namespace section
             vt_GamoverPanel.SetActive(vt_GameState == GameState.Gameover);
             vt_CreditPanel.SetActive(vt_GameState == GameState.Credit);
             vt_Learn.SetActive(vt_GameState == GameState.Learn);
+            vt_Win.SetActive(vt_GameState == GameState.GameWin);
             //vt_GameoverPanel.SetActive(vt_GameState == GameState.Gameover);
         }
 
